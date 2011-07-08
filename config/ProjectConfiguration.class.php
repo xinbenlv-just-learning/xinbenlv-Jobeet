461b199a1d87595ff9b570e9f95c72f49768482e
Zaian Victor Zhou <xinbenlv@gmail.com>
2011-07-07 20:24:44 -0700

    Generated project jobeet and frontend, configured to visit by broswer

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/usr/share/php/symfony/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
