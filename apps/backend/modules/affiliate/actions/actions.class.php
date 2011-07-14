40696cbe11bc730d289ba3c3c270e98eece21b82
Zaian Victor Zhou <xinbenlv@gmail.com>
2011-07-14 15:09:14 +0800

    Added mailer

--- apps/backend/modules/affiliate/actions/actions.class.php
+++ apps/backend/modules/affiliate/actions/actions.class.php
@@ -17,10 +17,28 @@ class affiliateActions extends autoAffiliateActions
 {
   public function executeListActivate()
   {
-    $this->getRoute()->getObject()->activate();
+    $affiliate = $this->getRoute()->getObject();
+    $affiliate->activate();
+ 
+    // send an email to the affiliate
+    $message = $this->getMailer()->compose(
+      array('jobeet@example.com' => 'Jobeet Bot'),
+      $affiliate->getEmail(),
+      'Jobeet affiliate token',
+      <<<EOF
+Your Jobeet affiliate account has been activated.
+ 
+Your token is {$affiliate->getToken()}.
+ 
+The Jobeet Bot.
+EOF
+    );
+ 
+    $this->getMailer()->send($message);
  
     $this->redirect('jobeet_affiliate');
   }
+
  
   public function executeListDeactivate()
   {
--- test/functional/backend/affiliateActionsTest.php
+++ test/functional/backend/affiliateActionsTest.php
@@ -1,19 +1,23 @@
 <?php
-
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
-
-$browser = new sfTestFunctional(new sfBrowser());
-
+ 
+$browser = new JobeetTestFunctional(new sfBrowser());
+$browser->loadData();
+ 
 $browser->
-  get('/affiliate/index')->
-
-  with('request')->begin()->
-    isParameter('module', 'affiliate')->
-    isParameter('action', 'index')->
-  end()->
-
-  with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
+  info('1 - Authentication')->
+  get('/affiliate')->
+  click('Signin', array(
+    'signin' => array('username' => 'admin', 'password' => 'admin'),
+    array('_with_csrf' => true)
+  ))->
+  with('response')->isRedirected()->
+  followRedirect()->
+ 
+  info('2 - When validating an affiliate, an email must be sent with its token')->
+  click('Activate', array(), array('position' => 1))->
+  with('mailer')->begin()->
+    checkHeader('Subject', '/Jobeet affiliate token/')->
+    checkBody('/Your token is symfony/')->
   end()
 ;
